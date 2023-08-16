edbe55c2d7c657be6a4dd33d1e73d60a40a30861
David Pallarés <serallapdivad@gmail.com>
2023-08-16 16:41:38 +0100

    Added styles and a loading screen when is fetching the data

--- src/componentes/ListOnePiece.jsx
+++ src/componentes/ListOnePiece.jsx
@@ -1,21 +1,44 @@
 import React, { useEffect, useState } from 'react'
 import OnePieceCharacters from './OnePieceCharacters'
+import { Link } from 'react-router-dom'
 
 const ListOnePiece = () => {
   const [characters, setCharacters] = useState([])
+  const [loading, setLoading] = useState(false)
 
   useEffect(() => {
+    setLoading(true)
     fetch('https://api.api-onepiece.com/characters')
       .then((response) => response.json())
       .then((data) => setCharacters(data))
+      .then(() => setLoading(false))
   }, [])
 
   return (
     <>
+      <h1 className='title-characters'>One Piece Characters</h1>
+      <div className='upper-buttons'>
+        <Link to='/'>
+          <button className='top-button'>Back</button>
+        </Link>
+      </div>
+
       <div className='presentacion'>
-        {characters.map((data, index) => (
-          <OnePieceCharacters key={index} index={index} data={data} />
-        ))}
+        {loading ? (
+          <h1>LOADING</h1>
+        ) : (
+          characters.map((data, index) =>
+            !data.size ||
+            !data.age ||
+            !data.bounty ||
+            !data.status ||
+            !data.job ? (
+              false
+            ) : (
+              <OnePieceCharacters key={index} index={index} data={data} />
+            )
+          )
+        )}
       </div>
     </>
   )
--- src/componentes/OnePieceCharacters.jsx
+++ src/componentes/OnePieceCharacters.jsx
@@ -4,20 +4,26 @@ const OnePieceCharacters = ({ data }) => {
   return (
     <>
       <div className='presentacion'>
-        <div className='personaje'>
+        <div className='usuario'>
+          <div className='character-pic'>
+            <img
+              src={`https://robohash.org/${data.french_name.toLowerCase()}.png?bgset=bg1`}
+              alt='OnePieceRandomizeeRobotProfilePic'
+            />
+          </div>
           <div className='datos'>
             <div className='nombre'>Name: {data.french_name}.</div>
-            <div>Size: ({data.size})</div>
-            <div>Age: ({data.age})</div>
-            <div>Bounty: ({data.bounty})</div>
-            <div>Status: ({data.status})</div>
-            <div>Job: ({data.job})</div>
-            <div>Birthday: ({data.birthday})</div>
+            <div>Size: {data.size}</div>
+            <div>Age: {data.age}</div>
+            <div>Bounty: {data.bounty}</div>
+            <div>Status: {data.status}</div>
+            <div>Job: {data.job}</div>
           </div>
-          {/* <div className='botones'>
-            <button className='rojo'>Ocultar</button>
-            <button className='verde'>Guardar</button>
-          </div> */}
+          <div className='botones'>
+            <button onClick={() => seeLore()} className='blue'>
+              LORE
+            </button>
+          </div>
         </div>
       </div>
     </>
